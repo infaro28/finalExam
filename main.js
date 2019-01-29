cfaf518a0456f1c4463a4b4a9448c31785f95c2b
Priscilla Infante <U91607@ust-global.com>
2019-01-29 10:17:46 -0600

    progress

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var products = [];
+var orderSumary = [];
 
 function table(){
     
@@ -21,6 +22,58 @@ function table(){
         price.innerText = products[i].price;
         listItem.appendChild(price);
         price.classList.add("col");
+
+        dataList.appendChild(listItem);  
+        i = i + 1; // alternative i++
+        
+    }
+
+   
+}
+
+function add(){
+    var productsName= document.getElementById('productsName').value
+    var productsAmount = parseInt(document.getElementById('productsAmount').value)
+    students.push({id: studentID, name: productsName, score: productsAmount})
+    scores.push(productsAmount)
+
+    sumary()
+}
+
+function Sumary(){
+    
+    var i = 0;
+    let dataList =  document.getElementById("dataList");
+    dataList.innerHTML=''
+    while(i < students.length){
+       
+        var listItem = document.createElement("section");
+
+        console.log(students[i]);
+        listItem.classList.add("row");
+
+        var id = document.createElement("section");
+        id.innerText = students[i].id;
+        listItem.appendChild(id);
+        id.classList.add("col");
+        
+        var name = document.createElement("section");
+        name.innerText = students[i].name;
+        listItem.appendChild(name);
+        name.classList.add("col");
+
+        var score = document.createElement("section")
+        score.innerText = students[i].score;
+        listItem.appendChild(score);
+        score.classList.add("col");
+
+        
+
+        if(students[i].score < 60){
+            score.classList.add("lowscore");
+
+            
+        }
 
         dataList.appendChild(listItem);  
         i = i + 1; // alternative i++
