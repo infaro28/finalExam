4b985b5c7b002b6f1b8aa8f0608f7324ba86ec36
Priscilla Infante <U91607@ust-global.com>
2019-01-29 15:06:27 -0600

    meh

--- main.js
+++ main.js
@@ -36,68 +36,69 @@ function add(){
     let sumaryTable =  document.getElementById("sumaryTable");
     let AmountID =  document.getElementById("AmountID");
     var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    //sumaryTable.push({Amount: AmountID})
     AmountID.innerText= AmountInput
     Sumary()
+
     mult()
 }
-
-function add1(){
-
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID1 =  document.getElementById("AmountID1");
-    var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    //sumaryTable.push({Amount: AmountID})
-    AmountID1.innerText= AmountInput
-    Sumary()
-    mult()
-}
-function add(){
-
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID =  document.getElementById("AmountID");
-    var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    //sumaryTable.push({Amount: AmountID})
-    AmountID.innerText= AmountInput
-    Sumary()
-    mult()
-}
-function add(){
-
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID =  document.getElementById("AmountID");
-    var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    //sumaryTable.push({Amount: AmountID})
-    AmountID.innerText= AmountInput
-    Sumary()
-    mult()
-}
-function add(){
-
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID =  document.getElementById("AmountID");
-    var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    //sumaryTable.push({Amount: AmountID})
-    AmountID.innerText= AmountInput
-    Sumary()
-    mult()
-}function add(){
-
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID =  document.getElementById("AmountID");
-    var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    //sumaryTable.push({Amount: AmountID})
-    AmountID.innerText= AmountInput
-    Sumary()
-    mult()
-}
-
-
 function Sumary(){
     let sumaryTable =  document.getElementById("sumaryTable");
     let AmountID =  document.getElementById("AmountID"); 
 }
 
+function add1(){
+    let sumaryTable =  document.getElementById("sumaryTable");
+    let AmountID1 =  document.getElementById("AmountID1");
+    var AmountInput = parseInt(document.getElementById('AmountInput').value)
+    AmountID1.innerText= AmountInput
+    Sumary1()
+    
+}
+function Sumary1(){
+    let sumaryTable =  document.getElementById("sumaryTable");
+    let AmountID1 =  document.getElementById("AmountID1"); 
+}
+
+function add2(){
+
+    let sumaryTable =  document.getElementById("sumaryTable");
+    let AmountID2 =  document.getElementById("AmountID2");
+    var AmountInput = parseInt(document.getElementById('AmountInput').value)
+    AmountID2.innerText= AmountInput
+    Sumary2()
+
+}
+function Sumary2(){
+    let sumaryTable =  document.getElementById("sumaryTable");
+    let AmountID2 =  document.getElementById("AmountID2"); 
+}
+
+function add3(){
+
+    let sumaryTable =  document.getElementById("sumaryTable");
+    let AmountID3 =  document.getElementById("AmountID3");
+    var AmountInput = parseInt(document.getElementById('AmountInput').value)
+    AmountID3.innerText= AmountInput
+    Sumary3()
+    
+}
+function Sumary3(){
+    let sumaryTable =  document.getElementById("sumaryTable");
+    let AmountID3 =  document.getElementById("AmountID3"); 
+}
+
+function add4(){
+
+    let sumaryTable =  document.getElementById("sumaryTable");
+    let AmountID4 =  document.getElementById("AmountID4");
+    var AmountInput = parseInt(document.getElementById('AmountInput').value)
+    AmountID4.innerText= AmountInput
+    Sumary4()
+}   
+function Sumary4(){
+    let sumaryTable =  document.getElementById("sumaryTable");
+    let AmountID4 =  document.getElementById("AmountID4"); 
+}
 
 
 function mult(AmountID,PriceID) {
@@ -108,6 +109,7 @@ function total() {
     alert('You hit!');
 }
 
-function toBuy(){
-    alert('Your order is confirmed');
+function toBUY() {
+    alert('You hit!');
 }
+
