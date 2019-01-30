3908f77e084deda1b5a4098cfd3bbaeacb0a6141
Priscilla Infante <U91607@ust-global.com>
2019-01-30 15:51:42 -0600

    botones

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var products = [];
 //var orderSumary = [];
 
+function add(){
+    var i = 0
+    while(i < productos.length){
+        var product = document.getElementById(`${i}`);
+        productos[i].amount = parseInt(product.value)
+        i = i+1;
+    };
+};
+
 function table(){
-    
     var i = 0;
     let dataList =  document.getElementById("dataList");
     dataList.innerHTML=''
@@ -24,121 +32,48 @@ function table(){
         price.classList.add("col");
 
         dataList.appendChild(listItem);  
-        i = i + 1; // alternative i++
+        i = i + 1; // alternative i++   
+    } 
+}
+
+function tablemod(){
+    var i = 0;
+    let dataListmod =  document.getElementById("dataListmod");
+    dataListmod.innerHTML=''
+    while(i < productos.length){
+       
+        var listItem = document.createElement("section");
+
+        console.log(productos[i]);
+        listItem.classList.add("row");
+
+        var name = document.createElement("section");
+        name.innerText = productos[i].name;
+        listItem.appendChild(name);
+        name.classList.add("col-4");
         
-    }
+        var price = document.createElement("section");
+        price.innerText = productos[i].price;
+        listItem.appendChild(price);
+        price.classList.add("col-4");
 
-   
+        var amount = document.createElement("INPUT");
+        amount.setAttribute("type", "number");
+        listItem.appendChild(amount);
+        amount.classList.add("col-2");
+        amount.id = ( `${[i]}`)
+        
+        var subtotal = document.createElement("section");
+        subtotal.innerText = productos[i].amount*productos[i].price;
+        listItem.appendChild(subtotal);
+        subtotal.classList.add("col-2");
+
+
+        dataListmod.appendChild(listItem);  
+        i = i + 1; // alternative i++   
+    } 
 }
 
-function add(){
-
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID =  document.getElementById("AmountID");
-    var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    AmountID.innerText= AmountInput
-    Sumary()
-    mult()
-
+function check(){
+    alert("bye")
 }
-function Sumary(){
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID =  document.getElementById("AmountID"); 
-}
-
-function add1(){
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID1 =  document.getElementById("AmountID1");
-    var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    AmountID1.innerText= AmountInput
-    Sumary1()
-    
-}
-function Sumary1(){
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID1 =  document.getElementById("AmountID1"); 
-}
-
-function add2(){
-
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID2 =  document.getElementById("AmountID2");
-    var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    AmountID2.innerText= AmountInput
-    Sumary2()
-
-}
-function Sumary2(){
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID2 =  document.getElementById("AmountID2"); 
-}
-
-function add3(){
-
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID3 =  document.getElementById("AmountID3");
-    var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    AmountID3.innerText= AmountInput
-    Sumary3()
-    
-}
-function Sumary3(){
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID3 =  document.getElementById("AmountID3"); 
-}
-
-function add4(){
-
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID4 =  document.getElementById("AmountID4");
-    var AmountInput = parseInt(document.getElementById('AmountInput').value)
-    AmountID4.innerText= AmountInput
-    Sumary4()
-}   
-function Sumary4(){
-    let sumaryTable =  document.getElementById("sumaryTable");
-    let AmountID4 =  document.getElementById("AmountID4"); 
-}
-
-
-function mult(AmountID,PriceID) {
-    let ttL = (parseInt(AmountID) * parseInt(PriceID));
-    return ttL
-}
-
-function show(){
-    
-    let ttL = mult();
-    console.log(ttL)
-    totalID.innerText = ttL
-
-}
-
-function total() {
-    alert('Total to pay');
-    
-}
-
-function toBuy() {
-    alert('Your purchase is confirmed');
-    let AmountID =  document.getElementById("AmountID");
-    AmountID.innerText= " "
-
-    alert('Your purchase is confirmed');
-    let AmountID1 =  document.getElementById("AmountID1");
-    AmountID1.innerText= " "
-
-    alert('Your purchase is confirmed');
-    let AmountID2 =  document.getElementById("AmountID2");
-    AmountID2.innerText= " "
-
-    alert('Your purchase is confirmed');
-    let AmountID3 =  document.getElementById("AmountID3");
-    AmountID3.innerText= " "
-
-    alert('Your purchase is confirmed');
-    let AmountID4 =  document.getElementById("AmountID4");
-    AmountID4.innerText= " "
-
-}
-
--- webservices.js
+++ webservices.js
@@ -1,6 +1,10 @@
+productos=[];
+order=[];
+
 const URL = "https://products-catalog-8e792.firebaseio.com/set-1.json";
 
 fetchData();
+fetchData2();
 
 function fetchData(){
 
@@ -13,18 +17,39 @@ function fetchData(){
     if (request.status !== OK) {
         document.innerHTML = 'An error occurred during your request: ' +  request.status + ' ' + request.statusText;
         return;
-      }
-
-      var remoteProduct = JSON.parse(request.responseText);
-
-      remoteProduct.forEach(element => {
-          products.push(element);
-      });
-     
-     table();
-
+    }
+    var remoteProduct = JSON.parse(request.responseText);
+    remoteProduct.forEach(element => {
+      element.amount = 0
+      products.push(element);
+    });
+    
+    table();
     };
-
    request.send();
+
+} 
+
+function fetchData2(){
+
+  var request = new XMLHttpRequest();
+  request.open('GET', URL, true);
+
+  request.onload = function() {
+  const OK = 200;
+
+  if (request.status !== OK) {
+      document.innerHTML = 'An error occurred during your request: ' +  request.status + ' ' + request.statusText;
+      return;
+  }
+  var remoteProduct = JSON.parse(request.responseText);
+  remoteProduct.forEach(element => {
+    element.amount = 0
+    productos.push(element);
+  });
+  tablemod();
+ 
+  };
+ request.send();
 
 } 
